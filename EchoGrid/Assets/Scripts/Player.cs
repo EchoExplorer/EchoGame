8a7cf384f746f8bafc6ea9d574e2adb497062c93
Rudina Morina <rudinamorina@Rudinas-MacBook-Pro.local>
2015-11-26 02:33:01 -0500

    Most updated version with echoes integrated

--- EchoGrid/Assets/Scripts/Player.cs
+++ EchoGrid/Assets/Scripts/Player.cs
@@ -8,7 +8,20 @@ public class Player : MovingObject {
 	public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
 	private Animator animator;					//Used to store a reference to the Player's animator component.
 	private Vector2 touchOrigin = -Vector2.one;
-	
+
+	private float touchTime = 0f;
+	private float minSwipeDist = 100f;
+
+	public AudioClip echo1m;
+	public AudioClip echo2m;
+	public AudioClip echo3m;
+	public AudioClip echo4m;
+	public AudioClip echo5m;
+	public AudioClip echo6m;
+	public AudioClip echo7m;
+	public AudioClip wallHit;
+
+	//public bool soundPlaying = false;
 	protected override void Start ()
 	{
 		//Get a component reference to the Player's animator component
@@ -18,6 +31,44 @@ public class Player : MovingObject {
 		base.Start ();
 	}
 
+	private void PlayEcho(int xPos) {
+		switch (xPos) 
+		{
+			
+			case 0:
+				SoundManager.instance.PlaySingle(echo7m);
+				Debug.Log ("7m");
+				break;
+			case 1:
+				SoundManager.instance.PlaySingle(echo6m);
+				Debug.Log ("6m");
+				break;
+			case 2:
+				SoundManager.instance.PlaySingle(echo5m);
+				Debug.Log ("5m");
+				break;
+			case 3:
+				SoundManager.instance.PlaySingle(echo4m);
+				Debug.Log ("4m");
+				break;
+			case 4:
+				SoundManager.instance.PlaySingle(echo3m);
+				Debug.Log ("3m");
+				break;
+			case 5:
+				SoundManager.instance.PlaySingle(echo2m);
+				Debug.Log ("2m");
+				break;
+			case 6:
+				SoundManager.instance.PlaySingle(echo1m);
+				Debug.Log ("1m");
+				break;
+			default:
+				SoundManager.instance.PlaySingle(echo1m);
+				break;
+			
+		}
+	}
 
 	private void Update ()
 	{
@@ -41,6 +92,21 @@ public class Player : MovingObject {
 		{
 			vertical = 0;
 		}
+
+		if (Input.GetKeyDown("f")) {
+			Debug.Log ("time down");
+			Debug.Log (Time.time);
+		}
+
+		if (Input.GetKeyUp("f")) {
+			
+			Vector2 curPosition = transform.position;
+			int xPos = (int) Mathf.Ceil(curPosition.x);
+			PlayEcho(xPos);
+			Debug.Log ("time UP");
+			Debug.Log (Time.time);	
+		}
+
 		//Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
 		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 		
@@ -49,12 +115,14 @@ public class Player : MovingObject {
 		{
 			//Store the first touch detected.
 			Touch myTouch = Input.touches[0];
+
 			
 			//Check if the phase of that touch equals Began
 			if (myTouch.phase == TouchPhase.Began)
 			{
 				//If so, set touchOrigin to the position of that touch
 				touchOrigin = myTouch.position;
+				touchTime = Time.time;
 			}
 			
 			//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
@@ -73,17 +141,29 @@ public class Player : MovingObject {
 				touchOrigin.x = -1;
 				
 				//Check if the difference along the x axis is greater than the difference along the y axis.
-				if (Mathf.Abs(x) > Mathf.Abs(y))
+				if (Mathf.Abs(x) > Mathf.Abs(y) && Mathf.Abs(x) >= minSwipeDist)
+				{
 					//If x is greater than zero, set horizontal to 1, otherwise set it to -1
 					horizontal = x > 0 ? 1 : -1;
-				else
+					Debug.Log ("X direction swipe");
+				}
+				else if (Mathf.Abs(y) > Mathf.Abs(x) && Mathf.Abs(y) >= minSwipeDist) {
 					//If y is greater than zero, set horizontal to 1, otherwise set it to -1
 					vertical = y > 0 ? 1 : -1;
+					Debug.Log ("Y direction swipe");
+				}
+				else if (Mathf.Abs(Time.time - touchTime) > 0.25) {
+					Vector2 curPosition = transform.position;
+					int xPos = (int) Mathf.Ceil(curPosition.x);
+					PlayEcho(xPos);
+				}
+
 			}
 		}
 		
 		#endif //End of mobile platform dependendent compilation section started above with #elif
 		//Check if we have a non-zero value for horizontal or vertical
+
 		if(horizontal != 0 || vertical != 0)
 		{
 			AttemptMove<Wall> (horizontal, vertical);
@@ -106,6 +186,7 @@ public class Player : MovingObject {
 		//Set hitWall to equal the component passed in as a parameter.
 		Wall hitWall = component as Wall;
 		Debug.Log ("Hit the wall");
+		SoundManager.instance.PlaySingle(wallHit);
 	}
 		
 	//Restart reloads the scene when called.
