db1397fa202f2aadd1389b7c03b8be4c0f72bbfc
Wenyu Wu <wenyuwu2@illinois.edu>
2016-08-05 06:17:12 +0900

    updated server part

--- Server Scripts/cgi-bin/acceptLevelData.py
+++ Server Scripts/cgi-bin/acceptLevelData.py
@@ -1,32 +1,49 @@
- #!/usr/bin/python
+#!/usr/bin/python                                                                                               
 
 import sqlite3, cgi, cgitb
 
-# Create instance of FieldStorage
-form = cgi.FieldStorage()
+from Crypto.PublicKey import RSA
+from base64 import b64decode
 
-db = sqlite3.connect('/srv/sqlite/data/gameData')
+fOpen = open('/srv/maze/key.txt', 'r')
+longkey = bytes(fOpen.read())
+
+rsakey = RSA.importKey(b64decode(longkey))
+
+def decrypt(decryptThis):
+    raw_cipher_data = b64decode(decryptThis)
+    return unicode(rsakey.decrypt(raw_cipher_data))
+
+dbName = "gameData"
+
+db = sqlite3.connect('/srv/sqlite/data/' + dbName)
 cursor = db.cursor()
 
-userName = form.getvalue('userName')
-currentLevel = int(form.getvalue('currentLevel'))
-crashCount = int(form.getvalue('crashCount'))
-stepCount = int(form.getvalue('stepCount'))
-timeElapsed = float(form.getvalue('timeElapsed'))
-startTime = form.getvalue('startTime')
-endTime = form.getvalue('endTime')
+# Create instance of FieldStorage                                                                               
+levelForm = cgi.FieldStorage()
 
-cursor.execute('''INSERT INTO GameData(userName, currentLevel,crashCount,
-stepCount, timeElapsed, startTime, endTime, dateTimeStamp) 
-VALUES(?,?,?,?,?,?,?,CURRENT_TIMESTAMP)''', (userName,
-currentLevel,crashCount, stepCount, timeElapsed, startTime, endTime))
+userName = decrypt(levelForm.getvalue('userName'))
+currentLevel = int(decrypt(levelForm.getvalue('currentLevel')))
+trackCount = int(decrypt(levelForm.getvalue('trackCount')))
+crashCount = int(decrypt(levelForm.getvalue('crashCount')))
+stepCount = int(decrypt(levelForm.getvalue('stepCount')))
+timeElapsed = float(decrypt(levelForm.getvalue('timeElapsed')))
+startTime = decrypt(levelForm.getvalue('startTime'))
+endTime = decrypt(levelForm.getvalue('endTime'))
+asciiLevelRep = decrypt(levelForm.getvalue('asciiLevelRep'))
+levelRecord = levelForm.getvalue('levelRecord')
 
-db.commit() #changes are committed to database
+cursor.execute('''INSERT INTO LevelData(userName, currentLevel, trackCount, crashCount,
+stepCount, timeElapsed, startTime, endTime, asciiLevelRep, levelRecord, dateTimeStamp)                           
+VALUES(?,?,?,?,?,?,?,?,?,?,CURRENT_TIMESTAMP)''', (userName,
+currentLevel, trackCount, crashCount, stepCount, timeElapsed, startTime, endTime, asciiLevelRep, levelRecord))
+
+db.commit() #changes are committed to database                                                                  
 db.close()
 
 print "Content-type:text/json\r\n\r\n"
 print "{",
-keys = form.keys()
+keys = levelForm.keys()
 for key in keys:
-    print key, ":\"", form[key].value, "\"", ",",
+    print key, ":\"", levelForm[key].value, "\"", ",",
 print "}"
